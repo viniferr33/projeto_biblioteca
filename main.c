2f4437f973ea98db66ce31ae37fde4752d74d31d
viniferr33 <vini.ferr33@gmail.com>
2020-11-12 10:33:47 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,11 @@
+/* ### GRUPO 3 ###
+Felipe Moraes Marcello                  RA: 
+Vinicius Ferreira Gomes                 RA:
+Yuri Carlos Achnitz Batista Belinsky    RA: 200603
+*/
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
