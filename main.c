8ca1a775e23dad222026078e51ba8e1237fb66e8
Yuri Achnitz <yuri_achnitz@hotmail.com>
2020-11-16 19:40:30 -0300

    A execução está quase completamente funcional e quase pronta para envio

--- main.c
+++ main.c
@@ -33,17 +33,34 @@ typedef struct aluno
     struct info_livro tabela[4];
 } aluno;
 
+struct info_aluno
+{
+    char sigla;  //'L' - livre, 'E' - emprestado, 'R' - reservado (data da retirada -1 dia depois de devolver)
+    char RA[7];  //
+    int dia_ret; //dia max = 31
+    int mes_ret; //mes fev = 28
+    int dia_dev; //abril, junho, setembro, novembro = 30 dias
+    int mes_dev; //o resto = 31
+};
+
+typedef struct livro
+{
+    int reg; //gerado automaticamente
+    char titulo[80];
+    char autor[80];
+    struct info_aluno status[2];
+} livro;
+
 /*
 Funções genericas do algoritmo
 */
-
 void pause();
 void flush();
+char retorna_char();
 
 /*
 Funções relacionadas a manipulação da struct "Aluno"
 */
-
 void aloca_aluno(aluno **p, int qtd);
 void mostra_alunos(aluno *p, int qtd);
 void cadastra_aluno(aluno *p, int qtd);
@@ -52,43 +69,115 @@ void consulta_parcial(aluno *p, int qtd);
 int quantia_aluno(aluno **p);
 void grava_aluno(aluno *p, int qtd);
 
+/*
+Funções para a struct "livro"
+*/
+int quantia_livro();
+void cadastra_livro(livro *livros, int qtd);
+void gravar_livro(livro *livros);
+void consulta_livro(livro *livros, int qtd);
+int compara_string(char *principal, char *outra);
+void status_livro(struct info_aluno *status);
+void aloca_livro(livro **p, int qtd);
+
 // ## MAIN ##
 int main(int argc, char const *argv[])
 {
+    char opt = '\0';
+    char op = '\0';
+
     aluno *alunos = NULL;
-    int opt = -1;
     int qtd_a = quantia_aluno(&alunos);
+
+    int qtd_livro;
+    livro *livros = NULL;
+    aloca_livro(&livros, 1);
 
     do
     {
         system(clear);
-        printf("0 - Sair\n1 - Adicionar\n2 - Pesquisa total\n3 - Pesquisa Parcial\n");
-        scanf("%i", &opt);
-        flush();
+
+        printf("Ola, essa e a \033[0;35mBIBLIOTECA\033[0m\n\nVoce tem tres opcoes:\n[1] - Trabalhar com alunos\n[2] - Trabalhar com livros\n[0] - Sair");
+        opt = retorna_char();
 
         switch (opt)
         {
-        case 1:
-            aloca_aluno(&alunos, qtd_a + 1);
-            cadastra_aluno(alunos, qtd_a);
-            qtd_a++;
+        case '0':
             break;
 
-        case 2:
-            consulta_total(alunos, qtd_a);
+        case '1':
+            do
+            {
+                system(clear);
+                printf("ALUNOS\n\n[1] - Adicionar\n[2] - Pesquisa total\n[3] - Pesquisa Parcial\n[0] - Sair");
+                op = retorna_char();
+
+                switch (op)
+                {
+                case '0':
+                    break;
+
+                case '1':
+                    aloca_aluno(&alunos, qtd_a + 1);
+                    cadastra_aluno(alunos, qtd_a);
+                    qtd_a++;
+                    break;
+
+                case '2':
+                    consulta_total(alunos, qtd_a);
+                    break;
+
+                case '3':
+                    consulta_parcial(alunos, qtd_a);
+                    break;
+
+                default:
+                    printf("\n\n\033[0;31mDigite uma opcao valida.\033[0m\n\n");
+                    pause();
+                    break;
+                }
+            } while (op != '0'); //switch dos alunos
+            grava_aluno(alunos, qtd_a);
             break;
 
-        case 3:
-            consulta_parcial(alunos, qtd_a);
+        case '2':
+            do
+            {
+                system(clear);
+                printf("LIVROS\n\n[1] - Cadastrar livros\n[2] - Consultar livros\n[0] - Sair");
+
+                op = retorna_char();
+
+                switch (op)
+                {
+                case '0':
+                    break;
+
+                case '1':
+                    qtd_livro = quantia_livro();
+                    cadastra_livro(livros, qtd_livro);
+                    break;
+
+                case '2':
+                    qtd_livro = quantia_livro();
+                    consulta_livro(livros, qtd_livro);
+                    break;
+
+                default:
+                    printf("\n\n\033[0;31mDigite uma opcao valida.\033[0m\n\n");
+                    pause();
+                    break;
+                }
+            } while (op != '0'); //switch dos livros
             break;
 
         default:
+            printf("\n\n\033[0;31mDigite uma opcao valida.\033[0m\n\n");
+            pause();
             break;
         }
+    } while (opt != '0'); //switch de tudo
 
-    } while (opt != 0);
-
-    grava_aluno(alunos, qtd_a);
     return 0;
 } //MAIN
 
@@ -160,9 +249,9 @@ void cadastra_aluno(aluno *p, int qtd)
         (p + qtd)->tabela[i].sigla = 'L';
     }
     system(clear);
-    printf("\033[0;32m### Cadastro Realizado com sucesso!! ###\033[0m\n");
+    printf("\033[0;32m### Cadastro Realizado com sucesso!! ###\033[0m\n\n");
     printf("Nome:\t\033[0;36m%s\033[0;0m\t", (p + qtd)->nome);
-    printf("RA: \t\033[0;36m%s\033[0;0m\n", (p + qtd)->RA);
+    printf("RA: \t\033[0;36m%s\033[0;0m\n\n", (p + qtd)->RA);
 
     pause();
 }
@@ -206,7 +295,7 @@ Consulta Parcial - Função da Struct Aluno
 void consulta_parcial(aluno *p, int qtd)
 {
     system(clear);
-    printf("\nDigite o RA do aluno:\t");
+    printf("Digite o RA do aluno:\t");
     int aux_ra = 0;
     char RA[7];
     do
@@ -244,7 +333,7 @@ void consulta_parcial(aluno *p, int qtd)
     {
         if (strcmp((p + i)->RA, RA) == 0)
         {
-            printf("Nome:\t\033[0;36m%s\033[0;0m\t", (p + i)->nome);
+            printf("\nNome:\t\033[0;36m%s\033[0;0m\t", (p + i)->nome);
             printf("RA: \t\033[0;36m%s\033[0;0m\n", (p + i)->RA);
             printf("Livros emprestados: \t\033[0;36m%i\033[0;0m", (p + i)->emprestado);
 
@@ -267,7 +356,7 @@ void consulta_parcial(aluno *p, int qtd)
         }
     }
     if (flag == 0)
-        printf("\nO aluno referente a esse RA não existe!");
+        printf("\n\nO aluno referente a esse RA nao existe!\n");
     pause();
 }
 
@@ -304,7 +393,7 @@ Grava Aluno - Função da Struct Aluno
 void grava_aluno(aluno *p, int qtd)
 {
     FILE *fl = NULL;
-    if((fl = fopen("aluno.bin", "wb")) == NULL)
+    if ((fl = fopen("aluno.bin", "wb")) == NULL)
     {
         printf("\033[0;31mErro de gravacao!\033[0m");
         pause();
@@ -312,24 +401,250 @@ void grava_aluno(aluno *p, int qtd)
     else
     {
         fwrite(p, sizeof(aluno), qtd, fl);
-        printf("\033[0;32mSalvo com sucesso!\033[0m\n");
+        printf("\n\n\033[0;32mSalvo com sucesso!\033[0m\n");
         pause();
     }
-        
 }
 
 /*
-Flush - Função generica
- - Limpa o buffer de entrada principal (stdin).
+Quantifica a quantia de livros existente no arquivo de livros.
 */
-void flush()
+int quantia_livro()
 {
+    FILE *fptr = NULL;
+    long int cont = 1;
+
+    if ((fptr = fopen("livros.bin", "rb")) == NULL)
+        return cont;
+    else
+    {
+        fseek(fptr, 0, 2);
+        cont = ftell(fptr) / sizeof(livro);
+        fclose(fptr);
+        return (cont + 1);
+    }
+}
+
+/*
+É a preparação de terreno para gravar um livro no arquivo de livros.
+*/
+void cadastra_livro(livro *livros, int qtd)
+{
+    system(clear);
+
+    livros->reg = qtd;
+
+    printf("CADASTRO DE LIVROS\n\nRegistro de livro: %i", qtd);
+
+    printf("\n\nInsira o titulo do livro: ");
+    gets(livros->titulo);
+    flush();
+
+    printf("Insira o autor do livro: ");
+    gets(livros->autor);
+    flush();
+
+    livros->status->sigla = 'L';
+
+    gravar_livro(livros);
+}
+
+/*
+É a gravação de fato no arquivo de livros.
+*/
+void gravar_livro(livro *livros)
+{
+    FILE *fptr = NULL;
+
+    if ((fptr = fopen("livros.bin", "ab")) == NULL)
+    {
+        printf("\n\n\033[0;31mErro ao abrir arquivo de livros!\033[0m\n\n");
+        pause();
+    }
+    else
+        fwrite(livros, sizeof(livro), 1, fptr);
+
+    fclose(fptr);
+}
+
+/*
+Consulta o arquivo de livros de diversas formas, podendo procurar informações específicas.
+*/
+void consulta_livro(livro *livros, int qtd)
+{
+    FILE *fptr = NULL;
+    int i, achou;
+    char op, status, titulo[80];
+
+    if ((fptr = fopen("livros.bin", "rb")) == NULL)
+        printf("\n\n\033[0;31mErro ao abrir arquivo de livros!\033[0m\n\n");
+    else
+    {
+        do
+        {
+            achou = 0;
+            system(clear);
+            printf("CONSULTA DE LIVROS\n\n[1] - Todos os livros\n[2] - Por status\n[3] - Por titulo\n[0] - Sair");
+
+            op = retorna_char();
+
+            switch (op)
+            {
+            case '0':
+                break;
+
+            case '1': //todos os livros
+                for (i = 0; i < qtd - 1; i++)
+                {
+                    fseek(fptr, i * sizeof(livro), 0);
+                    fread(livros, sizeof(livro), 1, fptr);
+
+                    printf("\n\nRegistro de livro: %i\n\nTitulo: %s\nAutor: %s\n\n", livros->reg, livros->titulo, livros->autor);
+                    status_livro(livros->status);
+                }
+
+                printf("\n\n");
+                pause();
+                break;
+
+            case '2': //livros por status
+                printf("\n\nDigite o status desejado (\033[0;32mL - Livre\033[0m, \033[0;31mE - Emprestado\033[0m, \033[0;33mR - Reservado\033[0m): ");
+
+                scanf("%c", &status);
+                flush();
+
+                for (i = 0; i < qtd - 1; i++)
+                {
+                    fseek(fptr, i * sizeof(livro), 0);
+                    fread(livros, sizeof(livro), 1, fptr);
+
+                    if (tolower(livros->status->sigla) == tolower(status))
+                    {
+                        printf("\n\nRegistro de livro: %i\n\nTitulo: %s\nAutor: %s\n\n", livros->reg, livros->titulo, livros->autor);
+                        status_livro(livros->status);
+                        achou = 1;
+                    }
+                }
+
+                if (achou == 0)
+                {
+                    printf("\n\nNenhum livro encontrado.");
+                }
+
+                printf("\n\n");
+                pause();
+                break;
+
+            case '3': //livros por titulo
+                printf("\n\nDigite o titulo desejado: ");
+                gets(titulo);
+                flush();
+
+                for (i = 0; i < qtd - 1; i++)
+                {
+                    fseek(fptr, i * sizeof(livro), 0);
+                    fread(livros, sizeof(livro), 1, fptr);
+
+                    if (compara_string(titulo, livros->titulo) == 0)
+                    {
+                        printf("\n\nRegistro de livro: %i\n\nTitulo: %s\nAutor: %s\n\n", livros->reg, livros->titulo, livros->autor);
+                        status_livro(livros->status);
+                        achou = 1;
+                    }
+                }
+
+                if (achou == 0)
+                {
+                    printf("\n\nNenhum livro encontrado.");
+                }
+
+                printf("\n\n");
+                pause();
+                break;
+
+            default:
+                printf("\n\n\033[0;31mDigite uma opcao valida.\033[0m\n\n");
+                pause();
+                break;
+            }
+        } while (op != '0');
+
+        fclose(fptr);
+    } //funcionalidade do consulta_livro
+} //fim do consulta_livro
+
+/*
+Compara duas strings ignorando maiusculas e minusculas.
+Vale-se notar que a ênfase está no parâmetro "principal".
+*/
+int compara_string(char *principal, char *outra)
+{
+    int i;
+
+    if (strlen(principal) != strlen(outra))
+        return 1;
+
+    for (i = 0; i < strlen(principal); i++)
+    {
+        if (tolower(*(principal + i)) != tolower(*(outra + i)))
+            return 1;
+    }
+
+    return 0;
+}
+
+/*
+Acessa e trabalha com o status de um livro.
+*/
+void status_livro(struct info_aluno *status)
+{
+    if (status->sigla == 'L')
+    {
+        printf("Status: \033[0;32mLivre\033[0m\n");
+    }
+
+    if (status->sigla == 'E')
+    {
+        printf("Status: \033[0;31mEmprestado\033[0m\n");
+    }
+
+    if (status->sigla == 'R')
+    {
+        printf("Status: \033[0;33mReservado3[0m\n");
+    }
+}
+
+/*
+Alocação dinâmica para o ponteiro de struct livro.
+*/
+void aloca_livro(livro **p, int qtd)
+{
+    if ((*p = (livro *)realloc(*p, qtd * sizeof(livro))) == NULL)
+    {
+        printf("\n\n\033[0;31mErro para alocar livro\033[0m\n\n");
+        pause();
+        exit(1);
+    }
+}
+
+/*
+Retorna um char, no Windows usa-se o getch(), no Linux é um scanf() normal.
+*/
+char retorna_char()
+{
+    flush();
+    char op;
+    flush();
 // ## Esse trecho de código é executado durante a compilação ##
 #if defined(_WIN32) || defined(_WIN64) // Testa se o SO é Windows
-    fflush(stdin);                     // Caso o SO seja Windows ele executa o fflush
+    op = getch();
+    return op;
 #else
 #ifdef __linux // Testa se o SO é Linux
-    __fpurge(stdin);         // Caso o SO seja linux executa o fpurge
+    printf("\n\nInsira a operacao desejada: ");
+    scanf("%c", &op);
+    flush();
+    return op;
 #endif
 #endif
 }
